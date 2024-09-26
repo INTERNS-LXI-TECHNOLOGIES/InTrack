f77eff7fe07900c39dd74c1b52d634af598d4acc
ANLAVK <anilasuresh44@gmail.com>
2024-09-26 21:44:55 +0530

    use annotation

--- AchievementRewardsSystem/implementation/intrack/src/main/java/Reward.java
+++ AchievementRewardsSystem/implementation/intrack/src/main/java/Reward.java
@@ -4,7 +4,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-<<<<<<< HEAD
+
 public class Reward {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,32 +29,5 @@ public class Reward {
 
     
 }
-=======
-public class Reward{
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private long Id;
 
- private String name;
 
-public long getId() {
-    return Id;
-}
-
-public void setId(long id) {
-    Id = id;
-}
-
-public String getName() {
-    return name;
-}
-
-public void setName(String name) {
-    this.name = name;
-}
-
- 
-    
-
-}
->>>>>>> 412cd7732da674e6b77c4e57f37ab7d18735f68b
