0fe823cc3c8fd5b43730b37720f368a62315c269
Arjun7945 <arjunarjups111@gmail.com>
2024-09-26 19:22:02 +0530

    servlet project without annotation Completed

--- AchievementRewardsSystem/demoWork/demoservlet/src/main/java/com/lxisoft/demoservlet/JavaDemo.java
+++ AchievementRewardsSystem/demoWork/demoservlet/src/main/java/com/lxisoft/demoservlet/JavaDemo.java
@@ -5,12 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/JavaDemo")
 public class JavaDemo extends HttpServlet{
 
     public void doPost(HttpServletRequest request,HttpServletResponse response){
