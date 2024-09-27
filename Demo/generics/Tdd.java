39f5436107fac01b0af23480abf9421ddc654f55
Jayaram122 <jayaramjeeva4@gmail.com>
2024-09-27 10:48:12 +0530

    using generic project completed

---
+++ Demo/generics/Tdd.java
@@ -0,0 +1,10 @@
+public class Tdd
+{
+	public static void main(String ar[]){
+Car<Integer> car = new Car<>();
+car.colour="blue";
+car.name=1;
+System.out.println(car.colour);
+System.out.println(car.name);
+	}
+}
