00fe6cd94aed139bca1dac3b66c1fa81519d01a9
mageshlxi <magesh.m.e@lxisoft.com>
2024-10-08 16:45:42 +0530

    String apis Math apis completed

---
+++ Math/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Math.addExact(10, 20));
+        System.out.println(Math.incrementExact(10));
+        System.out.println(Math.negateExact(10));
+        System.out.println(Math.floorDiv(10,20));
+
+        System.out.println(Math.exp(10));
+        System.out.println(Math.pow(2, 3));
+        System.out.println(Math.abs(-10));
+
+        System.out.println(Math.max(10,20));
+
+        
+    }
+}
