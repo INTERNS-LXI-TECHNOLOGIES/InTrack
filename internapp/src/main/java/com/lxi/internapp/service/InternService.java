426875f60617bf22db40b870e9c389b657c23c7d
ANLAVK <anilasuresh44@gmail.com>
2024-10-18 23:32:19 +0530

    task 1 CURD completed using spring boot

---
+++ internapp/src/main/java/com/lxi/internapp/service/InternService.java
@@ -0,0 +1,51 @@
+package com.lxi.internapp.service;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.*;
+import org.springframework.stereotype.Service;
+import com.lxi.internapp.entity.InternEnity;
+import com.lxi.internapp.repository.InternRepository;
+
+@Service
+public class InternService {
+    @Autowired
+    InternRepository internRepository;
+
+public InternEnity create(InternEnity intern){
+    return internRepository.save(intern);
+}
+
+public List<InternEnity> getAll(){
+    return internRepository.findAll();
+}
+
+public Optional<InternEnity> getId(Long id){
+    return internRepository.findById(id);
+}
+
+public InternEnity updateIntern(Long id,InternEnity updateintern){
+Optional<InternEnity> updateDetail=internRepository.findById(id);
+
+if(updateDetail.isPresent()){
+    InternEnity existintern = updateDetail.get();
+    existintern.setName(updateintern.getName());
+    existintern.setAge(updateintern.getAge());
+    return internRepository.save(existintern);
+}
+else
+return null;
+}
+
+public void deleteIntern(Long id){
+Optional<InternEnity> deleteinter = internRepository.findById(id);
+
+if(deleteinter.isPresent()){
+    InternEnity existingIntern = deleteinter.get();
+     internRepository.delete(existingIntern);
+}
+else
+System.out.println("null");
+}
+}
