b73f4cbae1aeb11e14128c4d6f2f2130bd0c6e39
mageshlxi <magesh.m.e@lxisoft.com>
2024-10-18 06:27:10 +0530

    first task of rest completed

---
+++ internproject/src/main/java/com/divisosoft/Controllers/InternController.java
@@ -0,0 +1,57 @@
+package com.divisosoft.Controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.divisosoft.models.Intern;
+import com.divisosoft.services.InternService;
+
+@RestController
+@RequestMapping("/intern")
+public class InternController {
+
+    @Autowired
+    private InternService internService;
+
+    @GetMapping
+    public List<Intern> getInterns(){
+        return internService.getAllInterns();
+    }
+
+    @GetMapping("/{id}")
+    public Intern getInternById(@PathVariable Long id){
+        Optional<Intern>myInternOptional = internService.getIternsById(id);
+        if(myInternOptional!=null){
+            return myInternOptional.get();
+        }
+        else{
+            return null;
+        }
+    }
+
+    @PostMapping
+    public void createIntern(@RequestBody Intern intern){
+        internService.saveIntern(intern);
+    }
+
+    @PutMapping("/{id}")
+    public void updateInter(@PathVariable Long id,@RequestBody Intern myIntern){
+        internService.updatingIntern(id,myIntern);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteIntern(@PathVariable Long id){
+        internService.deletingIntern(id);
+    }
+
+}
---
+++ internproject/src/test/java/com/divisosoft/internproject/InternprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.divisosoft.internproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
