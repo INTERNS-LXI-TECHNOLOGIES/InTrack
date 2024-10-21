83cce673a61cf0ef467fcb308edc12ecdfcbd346
Avinashvn <avinashvnair007@gmail>
2024-10-21 21:28:29 +0530

    Connecting client and server using REST-API

---
+++ SpringBootProjects/intern/intern/src/main/java/com/example/intern/controller/InternController.java
@@ -0,0 +1,84 @@
+package com.example.intern.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.intern.model.Intern;
+import com.example.intern.service.InternService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+
+
+
+
+
+@RestController
+@RequestMapping("/api/intern/showall")
+
+
+public class InternController {
+@Autowired
+InternService internService;
+
+@GetMapping()
+public List<org.openapitools.client.model.Intern> getInterns() {
+    return internService.getAllResources();
+}
+
+@PostMapping("/create")
+public void createIntern(@RequestBody Intern intern) {
+    if (intern == null) {
+        throw new IllegalArgumentException("Intern object is missing in the request body.");
+    }
+    internService.createIntern(intern);
+    }
+
+
+   /*  @GetMapping()
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    
+    public String getMethodName(@RequestParam String param) {
+        return new String();
+    }*/
+
+   
+
+    @PutMapping("/update/{id}")
+    public void updateIntern(@PathVariable Long id, @RequestBody Intern entity) throws RuntimeException{
+        
+        internService.updateIntern(id, entity);
+    
+    }
+
+    @PutMapping("updateName/{name}")
+    public void putMethodName(@PathVariable String name, @RequestBody Intern entity) {
+        
+        internService.upIntern(name, entity);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delIntern(@PathVariable Long id ){
+        internService.deleteIntern(id);
+    
+    
+
+
+
+    
+}
+}
---
+++ server/intern/intern/src/main/java/com/example/intern/service/InternService.java
@@ -0,0 +1,85 @@
+package com.example.intern.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.intern.model.Intern;
+import com.example.intern.repository.InternRepository;
+
+
+@Service
+public class InternService {
+
+    @Autowired
+    private InternRepository internRepository;
+
+    public List<Intern> getAllResources()
+    {
+       List<Intern> result = null;
+        try{
+            result = internRepository.findAll();
+        }
+        catch(Exception e){
+            System.out.println("Exception When Calling");
+            e.printStackTrace();
+
+        } 
+
+        return result;
+
+    }
+    
+
+   /*  public List<Intern> getAllIntern(){
+        return internRepository.findAll();
+    }*/
+
+    public void createIntern(Intern intern){
+        internRepository.save(intern);
+    }
+
+    public void updateIntern(Long id, Intern intern){
+       
+        Intern user = internRepository.findById(id).orElse(null);
+
+        if(user!=null){
+            user.setName(intern.getName());
+            user.setAge(intern.getAge());
+
+             internRepository.save(user);
+        }
+    }
+
+    public void upIntern(String name, Intern intern){
+        Intern user = internRepository.findByName(name);
+
+        if(user != null){
+            user.setName(intern.getName());
+            user.setAge(intern.getAge());
+
+            internRepository.save(user);
+        }
+    }
+
+    public void deleteIntern(Long id){
+
+        Optional<Intern> user = internRepository.findById(id);
+
+        if(user.isPresent()){
+            Intern delIntern = user.get();
+
+            internRepository.delete(delIntern);
+        }
+    }
+
+    
+
+
+    
+
+
+    
+}
